6cbb1b2a5e87be2a90f93a10a6bce2fe489ad5e9
Emma Tyrrell <126344950+EmmaTyrrell@users.noreply.github.com>
2025-04-04 12:27:44 -0600

    Update min_max_scale.py

--- min_max_scale.py
+++ min_max_scale.py
@@ -7,8 +7,10 @@ def min_max_scale(data, min_val=None, max_val=None, feature_range=(0, 1)):
     d_min = np.nanmin(data) if min_val is None else min_val
     d_max = np.nanmax(data) if max_val is None else max_val
 
+    # if d_max == d_min:
+    #     raise ValueError("Min and max are equal — can't scale.")
     if d_max == d_min:
-        raise ValueError("Min and max are equal — can't scale.")
+        return np.full_like(data, feature_range[0], dtype=np.float32)
 
     a, b = feature_range
     scaled = (data - d_min) / (d_max - d_min)  # to [0, 1]
